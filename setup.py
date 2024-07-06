c0401fdf7568265d2c457fbe8dd65247905a0d96
JaswanthMannem <mannem.jaswanth1@gmail.com>
2024-07-06 18:21:54 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 #installing Local packages in virtual enivironments
+from setuptools import find_packages,setup
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author="mannem jaswanth",
+    author_email="mannem.jaswanth1@gmail.com",
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
